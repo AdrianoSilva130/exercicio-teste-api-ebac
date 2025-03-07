8595290e1f84dd83ae36f4444573270bad0ec960
Adriano Silva <adrianosilva130@gmail.com>
2025-03-07 10:10:46 -0300

    Editar e Deletar usando Faker

--- cypress/e2e/exercicio-api.cy.js
+++ cypress/e2e/exercicio-api.cy.js
@@ -68,7 +68,7 @@ describe('Testes da Funcionalidade Usuários', () => {
      
   });
 
-  it.only('Deve editar um usuário previamente cadastrado - PUT', () => {
+  it('Deve editar um usuário previamente cadastrado - PUT', () => {
   cy.preCadastro(faker.person.firstName(), faker.internet.email(), 'teste', 'true')
     .then(response => {
       let id = response.body._id
@@ -87,16 +87,8 @@ describe('Testes da Funcionalidade Usuários', () => {
 
   it('Deve deletar um usuário previamente cadastrado - DELETE', () => {
       
-      cy.request({
-      method: 'POST',
-      url:  'usuarios',
-      body: {
-        "nome": (faker.person.fullName()),
-        "email": (faker.internet.email()),
-        "password": "teste",
-        "administrador": "true"   
-      }
-}).then((response) => {
+  cy.preCadastro(faker.person.firstName(), faker.internet.email(), 'teste', 'true')
+  .then((response) => {
   expect(response.status).to.eq(201);
   let id = response.body._id
   cy.request('DELETE', `usuarios/${id}`)
